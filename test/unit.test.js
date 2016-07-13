6c60f05a407d428033fe8f3dfe6a06909388a56f
Stephan DeSouza <stephand91@gmail.com>
2016-07-12 21:57:22 -0400

    Use mocks and stubs in unit tests.

--- test/unit.test.js
+++ test/unit.test.js
@@ -1,15 +1,16 @@
+import should from 'should';
+import sinon from 'sinon';
+
 import ldapStrat from '../';
 import * as constants from './fixtures/constants';
-import mockLdapServer from './fixtures/ldap-server';
+import * as client from './fixtures/mock-ldap-client';
 import sessionUtil from './fixtures/session-util';
-import should from 'should';
 
 let verify = (data, done) => {
     return done(null, data);
 };
 
 describe('when imported', () => {
-
     it('should be a class', (done) => {
         should(ldapStrat).be.type('function');
         return done();
@@ -23,11 +24,9 @@ describe('when imported', () => {
         }).not.throw();
         return done();
     });
-
 });
 
 describe('when constructed', () => {
-
     it('with no parameters, should throw an error', (done) => {
         should(() => {
             let strat = new ldapStrat();
@@ -74,126 +73,100 @@ describe('when constructed', () => {
         }).not.throw();
         return done();
     });
-
 });
 
-describe('when authenticate is called', () => {
-    it('with bad server name, throw an error', (done) => {
-        let strat = new ldapStrat(
-            sessionUtil.getOptions({
-                'uidTag': 'uid',
-                'server': {
-                    'url': 'ldap://nosuchserver:389'
-                }
-            }),
-            verify
-        );
-        strat.authenticate({'body': {'username': 'testuser', 'password': 'test123'}})
-        .then(() => {
-            return done(new Error('expected an error'));
-        })
-        .catch((err) => {
-            should(err).have.property('code', 'ENOTFOUND');
-            return done();
-        });
-    });
-    it('with unresponsive host, throw an error', (done) => {
-        let strat = new ldapStrat(
-            sessionUtil.getOptions({
-                'uidTag': 'uid',
-                // This automatically tries to connect to localhost
-                'url': 'ldap://'
-            }),
-            verify
-        );
-        strat.authenticate({'body': {'username': 'testuser', 'password': 'test123'}})
-        .then(() => {
-            return done(new Error('expected an error'));
-        })
-        .catch((err) => {
-            should(err).have.property('code', 'ECONNREFUSED');
-            return done();
-        });
-    });
-});
+describe('for client methods', () => {
+    const strat = new ldapStrat(
+        sessionUtil.getOptions({
+            'uidTag': 'uid',
+            'url': `${constants.MOCK_SERVER_URL}:${constants.MOCK_SERVER_PORT}`
+        }),
+        verify
+    );
 
-describe('when authenticate is called', () => {
-
-    let mockServer = null;
-
-    // setup mock ldap server to test against
-    before((done) => {
-        mockServer = new mockLdapServer();
-        mockServer.start(constants.MOCK_SERVER_PORT)
-            .then(() => {
-                return done();
-            });
+    before(() => {
+        sinon.stub(ldapStrat, '_createLdapClient').returns(client.mockLdapClient);
     });
 
     after(() => {
-        if (!mockServer) {
-            return;
-        }
-        mockServer.close();
-        mockServer = null;
+        ldapStrat._createLdapClient.restore();
     });
 
-    let strat = null;
-
-    // setup a new strat to test with
     beforeEach(() => {
-        strat = new ldapStrat(
-            sessionUtil.getOptions({
-                'uidTag': 'uid',
-                'url': `${constants.MOCK_SERVER_URL}:${constants.MOCK_SERVER_PORT}`
-            }),
-            verify
-        );
+        client.mockLdapClient.bind.reset();
+        client.mockLdapClient.search.reset();
+        client.mockLdapClient.mockSearchRes.on.reset();
     });
 
-    afterEach(() => {
-        strat = null;
-    });
+    describe('when authenticate is called', () => {
+        it('should reject for bad server name error event', () => {
+            client.mockLdapClient.eventError = client.notFoundError;
+            let res = strat.authenticate({'body': {'username': 'testuser', 'password': 'test123'}});
+            client.mockLdapClient.eventError = null;
+            should(res).be.a.Promise();
+            should(client.mockLdapClient.bind.calledOnce).be.ok();
+            return should(res).be.rejectedWith({ 'code': 'ENOTFOUND' })
+        });
 
-    it('with no params, should throw an error', (done) => {
-        strat.authenticate()
-        .then(() => {
-            return done(new Error('expected an error'));
-        })
-        .catch(() => {
-            return done();
+        it('should reject for unresponsive host error event', () => {
+            client.mockLdapClient.eventError = client.connRefusedError;
+            let res = strat.authenticate({'body': {'username': 'testuser', 'password': 'test123'}});
+            client.mockLdapClient.eventError = null;
+            should(res).be.a.Promise();
+            should(client.mockLdapClient.bind.calledOnce).be.ok();
+            return should(res).be.rejectedWith({ 'code': 'ECONNREFUSED' });
+        });
+
+        it('with no params, should throw an error', () => {
+            let res = strat.authenticate();
+            should(res).be.a.Promise();
+            return should(res).be.rejectedWith({ 'message': 'Missing req parameter.' });
+        });
+
+        it('with null req, should throw an error', () => {
+            let res = strat.authenticate(null);
+            should(res).be.a.Promise();
+            return should(res).be.rejectedWith({ 'message': 'Missing req parameter.' });
+        });
+
+        it('with req, but no body, should throw an error', () => {
+            let res = strat.authenticate({});
+            should(res).be.a.Promise();
+            return should(res).be.rejectedWith({ 'message': 'Missing username or password parameters.' });
+        });
+
+        it('should not throw an error', () => {
+            let res = strat.authenticate({'body': {'username': 'testuser', 'password': 'test123'}});
+            should(res).be.a.Promise();
+            return should(res).be.fulfilled();
+        });
+
+        describe('when _bind is called', () => {
+            it('should bind and resolve a promise', () => {
+                let res = strat._bind(client.mockLdapClient, {'username': 'testuser', 'password': 'test123'});
+                should(res).be.a.Promise();
+                should(client.mockLdapClient.bind.calledOnce).be.ok();
+                return should(res).be.fulfilled();
+            });
+        });
+
+        describe('when _search is called', () => {
+            it('should search and resolve a promise', () => {
+                let res = strat._search(client.mockLdapClient, {});
+                should(res).be.a.Promise();
+                should(client.mockLdapClient.mockSearchRes.on.callCount).be.eql(4);
+                return should(res).be.fulfilled();
+            });
+
+            it('should reject a promise on search error', () => {
+                client.mockLdapClient.searchError = client.ldapSearchError;
+                let res = strat._search(client.mockLdapClient, {});
+                should(client.mockLdapClient.search.calledOnce).be.ok();
+                should(res).be.a.Promise();
+                should(client.mockLdapClient.mockSearchRes.on.callCount).be.eql(4);
+                client.mockLdapClient.searchError = null;
+                return should(res).be.rejectedWith({ code: 'LDAPSEARCHERR' });
+            });
         });
     });
-
-    it('with null req, should throw an error', (done) => {
-        strat.authenticate(null)
-        .then(() => {
-            return done(new Error('expected an error'));
-        })
-        .catch(() => {
-            return done();
-        });
-    });
-
-    it('with req, but no body, should throw an error', (done) => {
-        strat.authenticate({})
-        .then(() => {
-            return done(new Error('expected an error'));
-        })
-        .catch(() => {
-            return done();
-        });
-    });
-
-    it('should not throw an error', (done) => {
-        strat.authenticate({'body': {'username': 'testuser', 'password': 'test123'}})
-        .then(() => {
-            return done();
-        })
-        .catch((err) => {
-            return done(err);
-        });
-    });
-
-
 });
